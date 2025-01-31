fbb1a555d2420758b86529c0e75838fdccfe4654
garsetayusuf <garsetayusuf@gmail.com>
2025-02-01 03:38:21 +0700

    feat: add swagger to module

--- src/swagger-sync/swagger-sync.module.ts
+++ src/swagger-sync/swagger-sync.module.ts
@@ -1,8 +1,21 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { SwaggerSyncService } from './swagger-sync.service';
+import { SwaggerSyncConfig } from './interfaces/swagger-sync-config.interface';
+import { SWAGGER_SYNC_OPTIONS } from './constants/constants';
 
-@Module({
-  providers: [SwaggerSyncService],
-  exports: [SwaggerSyncService],
-})
-export class SwaggerSyncModule {}
+@Module({})
+export class SwaggerSyncModule {
+  static register(options: SwaggerSyncConfig): DynamicModule {
+    return {
+      module: SwaggerSyncModule,
+      providers: [
+        {
+          provide: SWAGGER_SYNC_OPTIONS,
+          useValue: options,
+        },
+        SwaggerSyncService,
+      ],
+      exports: [SwaggerSyncService],
+    };
+  }
+}
