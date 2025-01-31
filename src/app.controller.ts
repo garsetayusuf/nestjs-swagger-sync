2a1b2c149f20d4b5dc874b17c1c18acf2d01dd80
garsetayusuf <garsetayusuf@gmail.com>
2025-02-01 05:06:20 +0700

    refactor: remove un used code and change package version

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,21 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { SwaggerSyncService } from './swagger-sync/swagger-sync.service';
 
-@ApiTags('API')
 @Controller()
 export class AppController {
   constructor(private readonly swaggerSyncService: SwaggerSyncService) {}
 
   @Get('sync')
-  @ApiOperation({ summary: 'Sync Swagger docs with Postman' })
   async syncSwagger() {
     await this.swaggerSyncService.syncSwagger();
     return { message: 'Swagger documentation synced with Postman' };
   }
 
   @Get('hello')
-  @ApiOperation({ summary: 'Sample endpoint' })
   getHello() {
     return { message: 'Hello World!' };
   }
