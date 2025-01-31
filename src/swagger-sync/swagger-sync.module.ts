71e4ab6359803f9a54484f541b9ddcdb0c32b4b8
garsetayusuf <garsetayusuf@gmail.com>
2025-02-01 04:16:57 +0700

    refactor: make module global

--- src/swagger-sync/swagger-sync.module.ts
+++ src/swagger-sync/swagger-sync.module.ts
@@ -1,8 +1,9 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Global, Module } from '@nestjs/common';
 import { SwaggerSyncService } from './swagger-sync.service';
 import { SwaggerSyncConfig } from './interfaces/swagger-sync-config.interface';
 import { SWAGGER_SYNC_OPTIONS } from './constants/constants';
 
+@Global()
 @Module({})
 export class SwaggerSyncModule {
   static register(options: SwaggerSyncConfig): DynamicModule {
